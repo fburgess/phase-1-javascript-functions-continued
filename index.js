2b8c0506879ad1a66fcff2105a897cc35c5ef219
Fredrick Burgess <Fredrick.l.Burgess@gmail.com>
2022-02-01 19:26:42 -0500

    commit tree 236329d8391bfc1438b312b15e0accca59567dfc

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(actOne = "roller-skate"){
+    return `This Saturday, I want to ${actOne}!`;
+}
+
+function mondayWork(workAct = "go to the office"){
+    return `This Monday, I will ${workAct}.`;
+}
+   
+
+ function wrapAdjective(symbol) {
+     return function (phrase) {
+         return `You are ${symbol}${phrase}${symbol}!`;
+     }
+ }
